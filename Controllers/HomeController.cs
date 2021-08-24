e2dae42453158afd8d6253f8a61570dcf7f3aa15
Sebastian Guzdek <sebag93@gmail.com>
2021-08-24 17:08:44 +0200

    wyświetlanie drzewa pobranego z bazy danych - 2 poziomy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IDEO.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,31 @@ namespace IDEO.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDEOEntities db = new IDEOEntities();
         public ActionResult Index()
         {
+            var htmlstring = "";
+            var nodes = db.IdeoTree.Where(a => a.parent_id == 0).ToList();
+            var leafs = db.IdeoTree.Where(a => a.parent_id > 0).ToList();
+            foreach(var node in nodes)
+            {
+                if ((db.IdeoTree.Where(a=> a.parent_id.Equals(node.id)).Count())>0)
+                {
+                    htmlstring += "<li><span class=\"node\">" + node.name + "</span><ul class=\"nested\">";
+                    foreach(var leaf in leafs.Where(a=>a.parent_id.Equals(node.id)))
+                    {
+                        htmlstring += "<li>"+leaf.name+"</li>";
+                    }
+                    htmlstring += "</ul>";
+                }
+                else
+                {
+                    htmlstring += "<li>" + node.name + "</li>";
+                }
+            }
+            htmlstring += "</li>";
+            ViewBag.nodes = nodes;
+            ViewBag.htmlstring = htmlstring;
             return View();
         }
     }
