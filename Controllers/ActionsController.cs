f87ab72d4debb7abee19f37630b99ca4424f76e3
Sebastian Guzdek <sebag93@gmail.com>
2021-08-26 22:50:33 +0200

    edytowanie elementów struktury

--- Controllers/ActionsController.cs
+++ Controllers/ActionsController.cs
@@ -9,6 +9,7 @@ namespace IDEO.Controllers
     {
         private readonly IDEOEntities db = new IDEOEntities();
         private readonly List<string> list = new List<string>();
+        private readonly List<string> list2 = new List<string>();
         private bool Status;
         private string message;
 
@@ -21,7 +22,7 @@ namespace IDEO.Controllers
                 list.Add(i.name);
             }
             list.Insert(0, "IDEO TREE");
-            ViewBag.test = list;
+            ViewBag.Items = list;
             return View(new AddViewModel { ParentName = list[0] });
         }
 
@@ -36,19 +37,13 @@ namespace IDEO.Controllers
                 list.Add(i.name);
             }
             list.Insert(0, "IDEO TREE");
-            ViewBag.test = list;
-
+            ViewBag.Items = list;
             int parent_id = 0;
-            if(item.ParentName == "IDEO TREE")
-            {
-                parent_id = 0;
-            }
-            else
+            if(item.ParentName != "IDEO TREE")
             {
                 var findparentid = db.IdeoTree.First(a => a.name == item.ParentName);
                 parent_id = findparentid.id;
             }
-
             if (ModelState.IsValid)
             {
                 if (FindByName(item.Name))
@@ -107,7 +102,84 @@ namespace IDEO.Controllers
         // GET: Edit
         public ActionResult Edit()
         {
+            List<IdeoTree> items = db.IdeoTree.OrderBy(a => a.name).ToList();
+            foreach (var i in items)
+            {
+                list.Add(i.name);
+            }
+            list2.Insert(0, "nie zmieniaj");
+            list2.Insert(1, "IDEO TREE");
+            list2.AddRange(list);
+            ViewBag.Items = list;
+            ViewBag.Items2 = list2;
             return View();
+        }
+
+        // POST: Edit
+        [HttpPost]
+        public ActionResult Edit(EditViewModel item)
+        {
+            List<IdeoTree> items = db.IdeoTree.OrderBy(a => a.name).ToList();
+            foreach (var i in items)
+            {
+                list.Add(i.name);
+            }
+            list2.Insert(0, "nie zmieniaj");
+            list2.Insert(1, "IDEO TREE");
+            list2.AddRange(list); 
+            ViewBag.Items = list;
+            ViewBag.Items2 = list2;
+
+            int parent_id;
+            if (item.NewParentName == "IDEO TREE")
+            {
+                parent_id = 0;
+            }
+            else if (item.NewParentName == "nie zmieniaj")
+            {
+                var findparentid = db.IdeoTree.First(a => a.name == item.Name);
+                parent_id = findparentid.parent_id;
+            }
+            else
+            {
+                var findparentid = db.IdeoTree.First(a => a.name == item.NewParentName);
+                parent_id = findparentid.id;
+            }
+            if (ModelState.IsValid)
+            {
+                if (FindByName(item.NewName))
+                {
+                    message = "Element o podanej nazwie już istnieje w strukturze.";
+                }
+                else
+                {
+                    if (item.NewName == null)
+                    {
+                        item.NewName = item.Name;
+                    }
+                    using (IDEOEntities db = new IDEOEntities()) 
+                    { 
+                        IdeoTree itemtochange = db.IdeoTree.SingleOrDefault(a => a.name == item.Name);
+                        if(itemtochange != null) 
+                        { 
+                            itemtochange.name = item.NewName;
+                            itemtochange.parent_id = parent_id;
+                            db.SaveChanges();
+                            message = "Zmiany zostały zapisane pomyślnie.";
+                            Status = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                message = "Nieprawidłowe żądanie";
+                Status = false;
+            }
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+
+            return View(item);
         }
 
         internal bool FindByName(string name)
--- Models/ActionsViewModel.cs
+++ Models/ActionsViewModel.cs
@@ -13,7 +13,22 @@ namespace IDEO.Models
         [Display(Name = "Nazwa")]
         public string Name { get; set; }
 
-        [Display(Name = "Nazwa rodzica")]
+        [Display(Name = "Nazwa węzła")]
         public string ParentName { get; set; }
+    }
+
+    public class EditViewModel
+    {
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+
+        [Display(Name = "Nowa nazwa")]
+        public string NewName { get; set; }
+
+        [Display(Name = "Nazwa węzła")]
+        public string ParentName { get; set; }
+
+        [Display(Name = "Nazwa nowego węzła")]
+        public string NewParentName { get; set; }
     }
 }
